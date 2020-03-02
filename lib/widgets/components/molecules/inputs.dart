61fdb38036e54598c93821a16b930c92b851f096
gentom <gento.m.developer@gmail.com>
2020-03-02 23:13:39 +0900

    add MolMultiLineTextField molecule widget

--- lib/widgets/components/molecules/inputs.dart
+++ lib/widgets/components/molecules/inputs.dart
@@ -31,3 +31,34 @@ class MolOneLineTextField extends StatelessWidget {
     );
   }
 }
+
+class MolMultiLineTextField extends StatelessWidget {
+  final IconData _icon;
+  final String _hintText;
+  final String _errorText;
+  final bool validate;
+  final TextEditingController _controller;
+
+  MolMultiLineTextField(
+    this._icon,
+    this._hintText,
+    this._errorText,
+    this._controller,
+    {
+      this.validate
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return AtmTextField(
+      _icon,
+      _hintText,
+      _errorText,
+      6,
+      _controller,
+      obscureText: false,
+      validate: validate,
+    );
+  }
+}
