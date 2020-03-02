b642d19be8dad8adfd1e230bcd05bbd7a1ce8753
gentom <gento.m.developer@gmail.com>
2020-03-02 17:16:50 +0900

    implement NoteRepository concrete class

--- lib/repositories/note_repository.dart
+++ lib/repositories/note_repository.dart
@@ -1,8 +1,35 @@
 import 'package:broccoli/models/note.dart';
+import 'package:broccoli/resources/providers/note_provider.dart';
 
 abstract class NoteRepositoryInterface {
   List<Note> getNotes();
   void createNote(Note note);
   void updateNote(Note note);
   void deleteNote(Note note);
+}
+
+class NoteRepository implements NoteRepositoryInterface {
+  final NoteProviderInterface _noteProvider;
+
+  NoteRepository(this._noteProvider);
+
+  @override
+  List<Note> getNotes() {
+    return _noteProvider.getNotes();
+  }
+
+  @override
+  void createNote(Note note) {
+    return _noteProvider.createNote(note);
+  }
+
+  @override
+  void updateNote(Note note) {
+    return _noteProvider.updateNote(note);
+  }
+
+  @override
+  void deleteNote(Note note) {
+    return _noteProvider.deleteNote(note);
+  }
 }
